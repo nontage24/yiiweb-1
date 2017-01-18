15a34a8d2b109ab75fded3076cfe2f3ac38caf85
tehnn <tehnnn@gmail.com>
2017-01-18 15:00:10 +0700

    utehn-smartcare

--- frontend/views/patient/_form.php
+++ frontend/views/patient/_form.php
@@ -28,59 +28,70 @@ use yii\widgets\ActiveForm;
     </div>
 
     <div class="form-group">
-        <div class="col-md-4">
+        <div class="col-md-3">
             <?= $form->field($model, 'sex')->textInput(['maxlength' => true]) ?>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-3">
             <?= $form->field($model, 'birth')->textInput() ?>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-3">
             <?= $form->field($model, 'province')->textInput(['maxlength' => true]) ?>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-3">
             <?= $form->field($model, 'district')->textInput(['maxlength' => true]) ?>
         </div>
 
     </div>
 
     <div class="form-group">
-
-
-
-        <?= $form->field($model, 'subdistrict')->textInput(['maxlength' => true]) ?>
-
-        <?= $form->field($model, 'village_no')->textInput(['maxlength' => true]) ?>
-
-        <?= $form->field($model, 'village_name')->textInput(['maxlength' => true]) ?>
-
-        <?= $form->field($model, 'house_no')->textInput(['maxlength' => true]) ?>
+        <div class="col-md-3">
+            <?= $form->field($model, 'subdistrict')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-3">
+            <?= $form->field($model, 'village_no')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-3">
+            <?= $form->field($model, 'village_name')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-3">
+            <?= $form->field($model, 'house_no')->textInput(['maxlength' => true]) ?>
+        </div>
     </div>
 
     <div class="form-group">
-
-        <?= $form->field($model, 'lat')->textInput(['maxlength' => true]) ?>
-
-        <?= $form->field($model, 'lon')->textInput(['maxlength' => true]) ?>
+        <div class="col-md-6">
+            <?= $form->field($model, 'lat')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-6">
+            <?= $form->field($model, 'lon')->textInput(['maxlength' => true]) ?>
+        </div>
     </div>
 
 
     <div class="form-group">
-
-        <?= $form->field($model, 'typearea')->textInput() ?>
-
-        <?= $form->field($model, 'nation')->textInput(['maxlength' => true]) ?>
-
-        <?= $form->field($model, 'race')->textInput(['maxlength' => true]) ?>
-
-        <?= $form->field($model, 'religion')->textInput(['maxlength' => true]) ?>
-
-        <?= $form->field($model, 'mstatus')->textInput(['maxlength' => true]) ?>
+        <div class="col-md-4">
+            <?= $form->field($model, 'typearea')->textInput() ?>
+        </div>
+        <div class="col-md-2">
+            <?= $form->field($model, 'nation')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-2">
+            <?= $form->field($model, 'race')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-2">
+            <?= $form->field($model, 'religion')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-2">
+            <?= $form->field($model, 'mstatus')->textInput(['maxlength' => true]) ?>
+        </div>
     </div>
 
 
 
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <div class="col-md-6">
+            <?= Html::submitButton($model->isNewRecord ? 'เพิ่ม' : 'บันทึก', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        </div>
     </div>
 
     <?php ActiveForm::end(); ?>
