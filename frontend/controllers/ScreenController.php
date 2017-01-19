4811a0380103f09716e12b80bba6fbac3f4e6d12
tehnn <tehnnn@gmail.com>
2017-01-19 12:14:39 +0700

    utehn-smartcare

--- frontend/controllers/ScreenController.php
+++ frontend/controllers/ScreenController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace frontend\controllers;
+use frontend\models\Screen;
 
 class ScreenController extends \yii\web\Controller
 {
@@ -8,6 +9,22 @@ class ScreenController extends \yii\web\Controller
     
     public function actionIndex($id)
     {
+        if(\Yii::$app->request->isPost){
+            $adl = \Yii::$app->request->post('adl');
+            $q2 = \Yii::$app->request->post('q2');
+            
+            $model = new Screen();
+            $model->patient_id = $id;
+            $model->date_screen = date('Y-m-d');
+            $model->adl = $adl;
+            $model->q2=$q2;
+            
+            if($model->save()){
+                \Yii::$app->session->setFlash('success', 'บันทึกสำเร็จ');
+                $this->redirect(['patient/index']);
+            }
+                    
+        }
        
         return $this->render('index',[
             'id'=>$id
